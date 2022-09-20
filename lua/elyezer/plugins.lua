15f2b1ec464f0b0154f4fa1a354cd7a161a26963
Elyézer Rezende <elyezermr@gmail.com>
2022-09-20 15:50:21 -0400

    Better packer bootstrapping

--- lua/elyezer/plugins.lua
+++ lua/elyezer/plugins.lua
@@ -1,10 +1,16 @@
-local fn = vim.fn
-local cmd = vim.cmd
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  cmd('packadd packer.nvim')
+local ensure_packer = function()
+  local cmd = vim.cmd
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    cmd('packadd packer.nvim')
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
