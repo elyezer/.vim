734585beeb91a9b63e3579c49351255dad314ad8
Elyézer Rezende <elyezermr@gmail.com>
2022-11-28 00:39:49 -0500

    Use cmp_nvim_lsp.default_capabilities

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,4 +1,4 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
 
 lspconfig.bashls.setup{capabilities = capabilities}
